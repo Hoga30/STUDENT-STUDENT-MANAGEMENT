0017706bd88e675be74435e64c0f60322f488617
hoga30 <tecyrose30@gmail.com>
2024-03-13 11:33:44 +0200

    Features : connection to database

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT || 3000;
+const db_connection_string = process.env.MONGODB_URI;
+app.use(express.json());
+mongoose.connect(db_connection_string)
+.then(()=>{
+console.log(" successfully connected to DB...");
+})
+.catch((err)=>{
+console.log(err);
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
