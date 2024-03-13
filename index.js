28a61621bd483592588ad0babe4a684e68bc8705
hoga30 <tecyrose30@gmail.com>
2024-03-13 12:29:47 +0200

    new: index changes

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
+import StudentModel from './models/student.models';
 const app = express();
 const port = process.env.PORT || 3000;
 const db_connection_string = process.env.MONGODB_URI;
 app.use(express.json());
+app.post("/student/add",async(req,res)=>{
+    try{
+const addedStudent = await StudentModel.create(req.body)
+res.status(201).json({
+mesage: "Student added!",
+student: addedStudent})
+
+} catch(err){
+
+    console.log(err.message);
+    res.status(500).json({
+        message:"Error adding student"});
+}
+});
+
+
 mongoose.connect(db_connection_string)
 .then(()=>{
 console.log(" successfully connected to DB...");
